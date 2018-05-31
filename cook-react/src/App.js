48963e3a84ad667e42e74c8136c9468e2e466cf7
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2018-05-31 11:57:15 +0900

    画像にコメントできるように。

--- cook-react/src/App.js
+++ cook-react/src/App.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todo: [
+        { title: 'JavaScript覚える' } ,
+        { title: 'jQuery覚える' } ,
+        { title: 'ES2015覚える' } ,
+        { title: 'React覚える' }
+      ]
+    };
+    this.addTodo = this.addTodo.bind(this);
+  }
+
+  //新規追加
+  addTodo(){
+    //追加
+    this.state.todo.push({
+      title: this.refs.newText.value
+    });
+
+    //保存
+    this.setState({
+      todo : this.state.todo
+    });
+
+    //初期化
+    this.refs.newText.value = '';
+  }
+
+  //削除機能
+  deleteTodo(i){
+    //削除
+    this.state.todo.splice(i ,1);
+    //保存
+    this.setState({
+      todo : this.state.todo
+    });
+
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ようこそ</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <img src="2012-12-01 14.42.02.jpg" width="220" height="220" />
+        <h3>コメントする</h3>
+        <ul>
+          {this.state.todo.map((todo, i)=>{
+            return <li key={i}><input type="button" value="☓" onClick={() => this.deleteTodo(i)}/> {todo.title}</li>
+          })}
+        </ul>
+        <input type="text"  ref="newText"/>
+        <input type="button" value="追加" onClick={this.addTodo} />
       </div>
+
+
     );
   }
 }
