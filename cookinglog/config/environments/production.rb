50c9653859cfd7b6395b57014ad1fd9ef60475ce
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2018-06-02 19:42:09 +0900

    fixed assets

--- cookinglog/config/environments/production.rb
+++ cookinglog/config/environments/production.rb
@@ -31,7 +31,9 @@ config.webpacker.check_yarn_integrity = false
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  serve_static_files = true
+  config.assets.css_compressor = :scss
   config.assets.precompile += %w( top.scss )
 
 
