5a0b42bc1847f653b142ab71250a961fba978a81
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2018-06-02 15:54:08 +0900

    herokuデプロイ時にtop.cssもprecompileできるように。

--- cookinglog/config/environments/production.rb
+++ cookinglog/config/environments/production.rb
@@ -32,6 +32,8 @@ config.webpacker.check_yarn_integrity = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile += %w( top.scss )
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
